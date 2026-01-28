5918dde6e1ea1baf8ff063cae697ea436bc5b16d
Kali-2303554 <2303554@uad.ac.uk>
2026-01-28 18:08:52 +0000

    Minor changes for testing Lighting/Blindness mechanic
    Sadie:
    Sprite flips with direction
    Changed jump to Arrow Up
    Changed camera to follow character body
    
    Lighting:
    Added a CanvasModulate to make the entire scene dark
    
    Alex:
    Implemented a basic Alex CharacterBody2D
    Including a PointLight2D w/ texture that follows the character body
    Controls with WASD
    Has no collision and ignores gravity
    
    Background:
    Included a background texture for lighting tests

--- blindside/Scenes/Player/Sadie/player_sadie.gd
+++ blindside/Scenes/Player/Sadie/player_sadie.gd
@@ -11,7 +11,7 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
@@ -19,6 +19,14 @@ func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		
+		# Based on direction the sprite will flip
+		if direction > 0:
+			$Sprite2D.flip_h = false
+			
+		elif direction < 0:
+			$Sprite2D.flip_h = true
+	
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
