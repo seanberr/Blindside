eb09d67baf0d8714f0a872fb06fe8f10256b88f7
Sean_Bermingham <2301469@uad.ac.uk>
2026-02-10 15:48:09 +0000

    hooked up pressure plates to unlock door - puzzle 1

--- blindside/Scenes/Test Rooms/Objects/puzzle_door.gd
+++ blindside/Scenes/Test Rooms/Objects/puzzle_door.gd
@@ -1,4 +1,4 @@
-extends StaticBody2D
+extends Area2D
 
 ## Counter for how many pressure plates have been activated
 var counter = 0
--- blindside/Scenes/Test Rooms/Objects/testing_locked_door.gd
+++ blindside/Scenes/Test Rooms/Objects/testing_locked_door.gd
@@ -4,7 +4,53 @@ extends Area2D
 @export var scene_to_load : PackedScene
 @onready var sprite : AnimatedSprite2D = $AnimatedSprite2D
 
+@export var pressure_plate_one : Area2D
+@export var pressure_plate_two : Area2D
+
+
+var counter = 0
+
+
+
+func _pressure_plate_check(value: bool):
+	
+	## If the pressure plate is activated then:
+	if value == true:
+		
+		## Add one to pressure plate total
+		counter += 1
+		
+		## Check if the lock should be unlocked
+		_check_lock()
+		
+	elif value == false:
+		
+		counter -= 1	
+		
+func _open_door():
+	
+		## Debug code for testing
+		$CollisionShape2D.set_deferred("disabled", true)
+		$Sprite2D.flip_v = true
+	
+## Function to check the lock condition
+func _check_lock():
+	
+	## Currently checks if two pressure plates have been activated then:
+	if counter == 2:
+		
+		## Calls the function to open the door
+		unlock()
+	
+## Whenever a pressure plate is stepped on or off
+func _on_pressure_plate_is_activated(value: Variant) -> void:
+	## Calls the function to check the behaviour of the pressure plate
+	_pressure_plate_check(value)
+
 func _ready() -> void:
+	pressure_plate_one.get_node("PressurePlate").is_activated.connect(_on_pressure_plate_is_activated)
+	pressure_plate_two.get_node("PressurePlate").is_activated.connect(_on_pressure_plate_is_activated)
+	
 	if is_locked:
 		lock()
 	else:
@@ -21,14 +67,11 @@ func _physics_process(delta: float) -> void:
 		if Input.is_action_just_pressed("Player1_Interact"):
 			interact()
 			
-## Connects to a specific pressure plate
-## When the something moves onto or off the pressure plate it will react
-func _on_pressure_plate_is_activated(value: Variant) -> void:
-	if value == true:
-		unlock()
-
 func change_scene():
-	get_tree().change_scene_to_packed(scene_to_load)
+	if scene_to_load == get_tree().get_current_scene():
+		get_tree().change_scene_to_packed(scene_to_load)
+	else:
+		get_tree().reload_current_scene()
 	
 func lock():
 	is_locked = true
