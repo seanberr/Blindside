d5ee4d832d83ef5b3679cbce43e8cef9bef50548
Sean_Bermingham <sbermingham1@outlook.com>
2026-02-11 16:58:45 +0000

    implemented first puzzle room

--- blindside/Assets/PressurePlate.gd
+++ blindside/Assets/PressurePlate.gd
@@ -1,4 +1,4 @@
-extends Node
+extends Area2D
 
 ## Sets up the class to use as a Node
 class_name PressurePlate
@@ -6,21 +6,22 @@ class_name PressurePlate
 ## Allows both variables to be changed from the Inspector
 @export var interactable: bool = false
 
+var is_activated : bool = false
+
 ## Sets a signal to control other objects
-signal is_activated(value)
+signal is_pressed()
 
 ## If something moves onto the pressure plate
-func _on_area_2d_area_entered(area: Area2D) -> void:
-
-	is_activated.emit(true)
+func _on_area_entered(area: Area2D) -> void:
+	is_activated = true
+	is_pressed.emit()
 	
 	## Debug sprite
-	$"../Button".visible = false
+	$"Button".visible = false
 	
 ## If something moves off the pressure plate
-func _on_area_2d_area_exited(area: Area2D) -> void:
-	
-	is_activated.emit(false)
-	
-	## Debug sprite
-	$"../Button".visible = true
+func _on_area_exited(area: Area2D) -> void:
+	if !get_overlapping_areas():
+		is_activated = false
+		## Debug sprite
+		$"Button".visible = true
