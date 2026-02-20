00a2b082d7d7d54356d33e06e7a2318ea03236c1
Kali-2303554 <2303554@uad.ac.uk>
2026-02-20 18:11:39 +0000

    Puzzle Room Two Progress
    Changed sprites.
    Added fourth boulder.
    Changed end tree so disappear when needed.

--- blindside/Scenes/Levels/sisyphus_boulder.gd
+++ blindside/Scenes/Levels/sisyphus_boulder.gd
@@ -1,13 +1,12 @@
 extends RigidBody2D
 
 
-func _on_ample_hole_body_entered(body: Node2D) -> void:
-	
+func _on_sisyphus_hole_body_entered(body: Node2D) -> void:
 	# Disabled the interaction with player
 	set_collision_layer_value(7, false)
 	set_collision_mask_value(7, false)
 	mass = 10000
-	
+	print("I'm sad")
 	## Enable typical static collision with player
 	set_collision_layer_value(2, true)
-	set_collision_mask_value(2, true)
+	
