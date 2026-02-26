0b02e2dd8caee38740bfa65a003f1220ca0a7833
Sean_Bermingham <sbermingham1@outlook.com>
2026-02-26 13:06:00 +0000

    Update input_tooltip.gd

--- blindside/Scenes/Levels/input_tooltip.gd
+++ blindside/Scenes/Levels/input_tooltip.gd
@@ -2,5 +2,5 @@ extends Node
 
 func _ready() -> void:
 	await get_tree().create_timer(2).timeout
-	TooltipManager.display_tip("Control Sadie with WASD. Press E to interact.\nControl Alex with the arrow keys. Press CTRL to interact.\n", 12, Vector2(30,30))
+	TooltipManager.display_tip("Player 1: Control Sadie with WASD. Press E to interact.\nPlayer 2: Control Alex with the arrow keys. Press CTRL to interact.\n", 12, Vector2(30,30))
 	TooltipManager.display_tip("Press Esc to exit the game.\nPress B to toggle blindness on and off (debug).\n", 12, Vector2(30,130))
