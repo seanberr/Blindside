b9f1ec2efb96f164d793f4891abd640b2ffc83b3
Kali-2303554 <2303554@uad.ac.uk>
2026-03-02 19:29:39 +0000

    Menu framework
    Added main menu framework with:
    Play button which loads the first puzzle room
    Extras button
    Settings button with controllable volume sliders and fullscreen button
    Quit button that exits the game
    
    Audio:
    Added two other audio buses for controlling "SFX" and "Music" volume

---
+++ blindside/Scenes/Menu/main_menu.gd
@@ -0,0 +1,45 @@
+extends Node2D
+
+func _ready() -> void:
+	$"CenterContainer/Settings Menu/Fullscreen".button_pressed = true if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN else false
+	$"CenterContainer/Settings Menu/Main Volume".value = db_to_linear(AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Master")))
+	$"CenterContainer/Settings Menu/SFX Volume".value = db_to_linear(AudioServer.get_bus_volume_db(AudioServer.get_bus_index("SFX")))
+	$"CenterContainer/Settings Menu/Music Volume".value = db_to_linear(AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Music")))
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file(str("res://Scenes/Levels/puzzle_room_one.tscn"))
+
+
+func _on_extras_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_options_pressed() -> void:
+	$"CenterContainer/Main Buttons".visible = false
+	$"CenterContainer/Settings Menu".visible = true
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_back_pressed() -> void:
+	$"CenterContainer/Main Buttons".visible = true
+	$"CenterContainer/Settings Menu".visible = false
+
+
+func _on_fullscreen_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_MAXIMIZED)
+
+func _on_main_volume_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_linear(AudioServer.get_bus_index("Master"), value)
+
+
+func _on_music_volume_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_linear(AudioServer.get_bus_index("Music"), value)
+
+
+func _on_sfx_volume_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_linear(AudioServer.get_bus_index("SFX"), value)
