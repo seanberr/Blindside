850ef7031a036f418d175f32bed9d748910e9575
Kali-2303554 <2303554@uad.ac.uk>
2026-03-23 13:59:10 +0000

    Animation Scale hotfix

--- blindside/Scenes/Player/Sadie/animation_controller.gd
+++ blindside/Scenes/Player/Sadie/animation_controller.gd
@@ -9,13 +9,15 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	
+	print(player.name)
 	match player.state_machine.current_state.name:
 		"Ground Movement State":
 			sprite.play("Walk")
-			sprite.scale = Vector2(0.5, 0.5)
+			if player.name == "PlayerSadie":	
+				sprite.scale = Vector2(0.5, 0.5)
 		"Idle State":
 			sprite.play("Idle")
-			sprite.scale = Vector2(0.25, 0.25)
+			if player.name == "PlayerSadie":	
+				sprite.scale = Vector2(0.25, 0.25)
 		"Air Movement State":
 			sprite.play("Jump")
