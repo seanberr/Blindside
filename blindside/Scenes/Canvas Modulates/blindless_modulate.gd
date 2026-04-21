d060c4d74138d4a5d7b792080d5c6627e6d842ee
Kali-2303554 <2303554@uad.ac.uk>
2026-04-21 21:30:34 +0100

    Disabled Debug Blindness

--- blindside/Scenes/Canvas Modulates/blindless_modulate.gd
+++ blindside/Scenes/Canvas Modulates/blindless_modulate.gd
@@ -1,8 +1,9 @@
 extends CanvasModulate
 
 func _physics_process(delta: float) -> void:
-	if Input.is_action_just_pressed("Toggle Blindness"):
-		if visible:
-			visible = false
-		else:
-			visible = true
+	if Global.extras_unlocked:
+		if Input.is_action_just_pressed("Toggle Blindness"):
+			if visible:
+				visible = false
+			else:
+				visible = true
